579d9ff0451cc723f041a60ff2ec2738eacfd434
tangxuliang <1297441127@qq.com>
2019-05-29 06:55:56 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -9,5 +10,13 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    /**
+     * 显示用户个人信息的页面
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
