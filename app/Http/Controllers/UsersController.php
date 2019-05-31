fc9daa4b9626192e6fdfcbb9c2b09c79ce16ec36
txl <1297441127@qq.com>
2019-05-31 08:16:16 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -41,4 +41,34 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
+
+    /**
+     *  编辑用户个人资料的页面
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', ['user'=>$user]);
+    }
+
+    /**
+     *  更新用户
+     */
+    public function update(Request $request, User $user)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if(isset($request->password) && $request->password) {
+            $data['password'] = $request->password;
+        }
+
+        $user->update($data);
+
+        session()->flash('success', '更新资料成功');
+        return redirect()->route('users.show', $user->id);
+    }
 }
