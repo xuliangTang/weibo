950c99be3c45cc2aeab1bfcac652b0bb1f01c5be
txl <1297441127@qq.com>
2019-06-18 07:10:47 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,6 +27,17 @@ class StatusesController extends Controller
         ]);
 
         session()->flash('success', '发布成功。');
+        return redirect()->back();
+    }
+
+    /**
+     * 删除微博
+     */
+    public function destroy(Status $status)     // 隐性路由模型绑定 Laravel 会自动查找并注入对应 ID 的实例对象 $status
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
         return redirect()->back();
     }
 }
