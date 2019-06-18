f9a6ed84b496b189558159a71406e25bc708c1d4
txl <1297441127@qq.com>
2019-06-18 07:44:05 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,10 +57,46 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    // 用户粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 用户关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
     public function feed()
     {
         return $this->statuses()->orderBy('created_at', 'desc');
     }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if(!is_array($user_ids))
+            $user_ids = compact('user_ids');
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if(!is_array($user_ids))
+            $user_ids = compact('user_ids');
+        $this->followings()->detach($user_ids);
+    }
+
+    // 是否关注了某用户
+    public function isFollowings($user_id)
+    {
+        return $this->followings->contains($user_id);
+        // $this->followings()->get()
+    }
+
 
     public function gravatar($size = '100')
     {
